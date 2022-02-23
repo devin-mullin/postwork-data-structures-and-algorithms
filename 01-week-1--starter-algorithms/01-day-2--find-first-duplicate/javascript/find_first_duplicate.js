3cd84af010ba062dbd934018ecf34554cbd1f12d
Devin Mullin <devin.mullin90@gmail.com>
2022-02-23 15:12:25 -0600

    first duplicate complete

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,19 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let firstIndex
+  let isMatch = false
+  for(i = 0; i < arr.length; i++){
+    for(j = i + 1; j < arr.length; j++){
+      if(arr[i] === arr[j]){
+        firstIndex = arr[i];
+        isMatch = true
+    }
+  }
+}
+if(isMatch === true)
+return firstIndex
+else 
+return -1
+
 }
 
 if (require.main === module) {
@@ -17,3 +31,5 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// two indexes created by iterating over the array, compared for equality
